df5b127fba2b5b5beb11356761e05804057548d7
asouwn <asouwn@126.com>
2024-03-13 22:09:23 +0800

    Update! finished the ETCDRegistry but no test

--- Core/src/main/java/com/asouwn/core/registry/RegistryFactory.java
+++ Core/src/main/java/com/asouwn/core/registry/RegistryFactory.java
@@ -6,8 +6,6 @@ public class RegistryFactory {
     static {
         SpiLoader.load(Registry.class);
     }
-    private static final Registry DEFAULT_REGISTRY = new EtcdRegistry();
-
     public static Registry getInstance(String key){
         return SpiLoader.getInstance(Registry.class, key);
     }
